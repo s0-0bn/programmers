cf1f8e3ee97400941443253225a25405153f7285
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-01-18 14:22:34 +0900

    [level 2] Title: 올바른 괄호, Time: 7.44 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,17 @@
+def solution(s):
+    stk = []
+    if s[0]==")" or s[-1]=="(":
+        return False
+    
+    for i in s:
+        if i=="(":
+            stk.append("(")
+        else: 
+            if "(" in stk:
+                stk.pop()
+            else:
+                return False
+    if stk:
+        return False
+    else:
+        return True
