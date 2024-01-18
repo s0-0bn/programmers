f6470fecc2a36b31352caf499cf0dc94c20b55d8
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-01-18 13:07:41 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -3,6 +3,5 @@ def solution(numbers):
     nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
     for i,j in enumerate(nums):
-        if j in numbers:
-            numbers = numbers.replace(j,str(i))
+        numbers = numbers.replace(j,str(i))
     return int(numbers)
