e031a9c1f83c00be401edf055a1d245269b6ace3
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-01-18 15:33:51 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -8,3 +8,11 @@ def solution(spell, dic):
         if ''.join(j) in dic:
             return 1
     return 2
+
+
+def solution2(spell, dic):
+    spell = set(spell)
+    for s in dic:
+        if not spell-set(s):
+            return 1
+    return 2
