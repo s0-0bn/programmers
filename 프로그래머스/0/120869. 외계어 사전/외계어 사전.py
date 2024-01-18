26e891d2d22ae6410cd519528acd40bcb22fb53e
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-01-18 15:36:30 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -2,10 +2,7 @@ from itertools import permutations
 def solution(spell, dic):
     box = []
     for perm in permutations(spell,len(spell)):
-        box.append(perm)
-
-    for j in box:
-        if ''.join(j) in dic:
+        if ''.join(perm) in dic:
             return 1
     return 2
 
