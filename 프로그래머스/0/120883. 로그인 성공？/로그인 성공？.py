2605ef12024d3a04e8cba27b8fb777d69fb3f87b
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-02-19 16:44:28 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,4 +1,4 @@
-def solution(id_pw, db):
+def solution2(id_pw, db):
 
     id_lst = [i[0] for i in db]
     pw_lst = [i[1] for i in db]
@@ -7,5 +7,13 @@ def solution(id_pw, db):
         if id_pw[1] == pw_lst[id_lst.index(id_pw[0])]:
             return 'login'
         else:
+            return "wrong pw"
+    return "fail"
+
+def solution(id_pw, db):
+    for ID, PW in db:
+        if id_pw[0]==ID and id_pw[1]==PW:
+            return "login"
+        elif id_pw[0]==ID and id_pw[1]!=PW:
             return "wrong pw"
     return "fail"
