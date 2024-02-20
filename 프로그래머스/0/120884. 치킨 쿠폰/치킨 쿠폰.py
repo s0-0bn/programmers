b75772631a56d6aec0a9cc3a27310c88a8b739cc
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-02-20 17:21:05 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,15 @@
+def solution(chicken):
+    answer = 0
+    mod = 0
+    
+    while mod+chicken>=10:
+        answer+=chicken//10
+        mod = chicken%10
+        
+        chicken = mod+(chicken//10)
+        mod=0
+    return answer
+
+
+# 1999 199   208   20   28   2    10  1
+#       9           8        8     
