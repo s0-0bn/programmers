ccd174cd08d28ec0e07b730e3f9b4c6e65c8bbb8
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-02-25 16:49:15 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,10 @@
+def solution(dots):
+    def slope(first, second):
+        return(second[1]-first[1])/(second[0]-first[0])
+    
+    for i in range(3):
+        mydots=dots[:] 
+        print(mydots[i])
+        if slope(mydots.pop(), mydots.pop(i)) == slope(mydots.pop(), mydots.pop()):
+            return 1
+    return 0
