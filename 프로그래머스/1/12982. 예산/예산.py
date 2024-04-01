a9c429c955a61cf7e414829b23eb185d151d9d0e
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-04-01 17:27:40 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,48 +1,11 @@
+# 문제 똑바로 읽자^^
+# budget을 0으로 만드는 조합을 찾는게 아니라
+# budget 내에서 최대로 물품 지원할 수 있는 부서의 수를 찾는 것
 def solution(d, budget):
-    
+    answer = 0
     d.sort()
-    count = 0
     for i in d:
         if i <= budget:
-            count += 1
+            answer += 1
             budget -= i
-    return count
-    
-
-
-# 1 20 22 + 2 4 5 21 23 시간 초과 되는게 많음
-from itertools import combinations
-def solution2(d, budget):
-    answer = 0
-    box = []
-    for i in range(len(d),0,-1):
-        for j in combinations(d,i):
-            if sum(j)==budget:
-                return i
-    return i
-
-# 1 20 22 + 3 4 7 21 23 통과
-def solution3(d, budget):
-    if sum(d) == budget:
-        return len(d)
-    
-    answer = budget
-    d = sorted(d)
-    cnt = 0
-    while cnt<=len(d):
-        for i in range(cnt,len(d)):
-            answer-=d[i]
-            if answer==0:
-                return i-cnt+1
-            elif answer<0:
-                answer=budget
-                break   
-            print(answer)
-        cnt+=1
-    return 0
-
-'''
-1 3 5 6 7  /  10
-
-
-'''
+    return answer
