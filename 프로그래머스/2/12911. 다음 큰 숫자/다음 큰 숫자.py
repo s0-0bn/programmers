83f198afda3c55bb3063c80d3e61a037d28ab6d3
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-04-03 17:38:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,3 +1,6 @@
+#... .count가 된다....
+# bin_check를 만들 필요가 없었다
+# bin(n)[2:].count('1')
 def bin_check(n):
     bin_n = bin(n)[2:]
     cnt=0
@@ -9,6 +12,7 @@ def bin_check(n):
 
 
 def solution(n):
+    print(bin(n).count('1'))
     ans = bin_check(n)
     while True:
         box = bin_check(n+1)
