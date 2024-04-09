93371e3d1f93042c857817c1c053437dfbcd30a6
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-04-09 16:07:02 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+# with tab(
+#     select host_id
+#     from places
+#     group by host_id
+#     having count(host_id)>1
+# )
+
+SELECT id, name, host_id
+from places
+where host_id in ( select host_id
+    from places
+    group by host_id
+    having count(host_id)>1)
+order by id 
