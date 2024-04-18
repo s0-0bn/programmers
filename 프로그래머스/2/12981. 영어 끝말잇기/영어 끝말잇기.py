414aa4c0a3c31ec71c2771b07482d1bd9e3c4c8f
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-04-18 15:47:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,12 +1,7 @@
 def solution(n, words):
     box = [words[0]]
     for i in range(1,len(words)):
-        if words[i-1][-1] != words[i][0]:
+        if words[i-1][-1] != words[i][0] or words[i] in box:
             return [(i%n)+1, (i//n)+1]
-        
-        if words[i] in box:
-            print(i)
-            return [(i%n)+1, (i//n)+1]
-        
         box.append(words[i])
     return [0,0]
