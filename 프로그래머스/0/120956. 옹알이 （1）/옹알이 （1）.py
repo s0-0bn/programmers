1fc895ddd0e3efdc4fd2e3eefca149b3aeb38a56
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-04-19 13:28:06 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -7,16 +7,8 @@ def solution(babbling):
         for j in babe:
             if j in i:
                 i = i.replace(j,'%%')
-            
         box.append(i)
     for i in box:
         if set(i) == {"%"}:
             answer+=1
     return answer
-
-
-'''babbling의 단어가 4가지로 다 나눠줘야 함
-ayayee 가 나왔다. 하면, 일단 %% 으로 교체 해줬음
-지금 테스트1에서 wyeoo 때문에 문제가 발생했는데
-ye가 수정되면 woo 가 되서 이게 바뀌면 안되는데 바뀌고 있음
-그래서 이부분 수정해야함!! 우얄까 우얘'''
