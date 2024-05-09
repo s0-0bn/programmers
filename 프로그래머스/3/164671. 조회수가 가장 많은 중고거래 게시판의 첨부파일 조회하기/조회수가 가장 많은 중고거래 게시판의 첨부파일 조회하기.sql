0400917c964bf3eb65cffe66b77d20366ecfd0ad
s0-0bn <156975040+s0-0bn@users.noreply.github.com>
2024-05-09 16:11:08 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
-SELECT concat("/home/grep/src/",ugf.board_id,"/",ugf.file_id,ugf.file_name,ugf.file_ext)as file_path
-from used_goods_board as ugb join used_goods_file as ugf using(board_id)
+select concat("/home/grep/src/",ugf.board_id,"/",ugf.file_id,ugf.file_name,ugf.file_ext)as file_path
+from used_goods_board as ugb 
+    join used_goods_file as ugf using(board_id)
 where views = (select max(views)
-              from used_goods_board)
+               from used_goods_board)
 order by file_id desc
